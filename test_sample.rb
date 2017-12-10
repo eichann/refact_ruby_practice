d8f1a85da5b984dd9380ca50c1342bcee84b362a
eichann <k.abe.in401@gmail.com>
2017-12-10 16:13:36 +0900

    不要なコードをコメントアウト

--- test_sample.rb
+++ test_sample.rb
@@ -1,16 +1,16 @@
 
-require 'test/unit'
+# require 'test/unit'
 
-class Sample
-  def self.greeting
-    'Hello, World!'
-  end
-end
+# class Sample
+#   def self.greeting
+#     'Hello, World!'
+#   end
+# end
 
-class TestSample < Test::Unit::TestCase
-  def test_greeting
-    assert_equal 'Hello, World!', Sample.greeting
-  end
-end
+# class TestSample < Test::Unit::TestCase
+#   def test_greeting
+#     assert_equal 'Hello, World!', Sample.greeting
+#   end
+# end
 
 
